9028e90e0da527bfa29ee48a28d4650f0944de77
ilacinap <94383191+ilacinap@users.noreply.github.com>
2022-02-01 19:01:28 +0100

    Update helloPani.js

--- 2022_02_01/helloPani.js
+++ 2022_02_01/helloPani.js
@@ -1,7 +1,7 @@
 //
 //
 //
-let p = [ { x: 0.0, y: 12.0 }, { x: 5.0, y: 0.0 } ]
+let p = [ { x: 0.0, y: 0.0 }, { x: 2.0, y: 4.0 } ]
 console.log("Ben ritrovati! Iniziamo JavaScript!")
 d = (p[0].x - p[1].x)/(p[0].y - p[1].y)
 console.log("Il risultato è " + d)
