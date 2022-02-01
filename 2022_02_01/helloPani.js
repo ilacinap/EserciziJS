8ec5f55be02bdde4c60588d485d07b30836fcbc2
ilacinap <94383191+ilacinap@users.noreply.github.com>
2022-02-01 18:20:56 +0100

    coefficiente angolare

---
+++ 2022_02_01/helloPani.js
@@ -0,0 +1,8 @@
+//
+//
+//
+let p = [ { x: 0.0, y: 12.0 }, { x: 5.0, y: 0.0 } ]
+console.log("Ben ritrovati! Iniziamo JavaScript!");
+d = (p[0].x - p[1].x)/(p[0].y - p[1].y))
+console.log("Il risultato è " + d);
+
