4b8e0d6e656f1252ab9d2a9216137e2b67afa49c
ilacinap <94383191+ilacinap@users.noreply.github.com>
2022-02-15 16:56:33 +0100

    Add files via upload

--- esempio.js
+++ esempio.js
@@ -3,4 +3,21 @@ function chiamata(){
     let a=d.getAttribute("onload")
     alert("ciao")
     alert(a)
-}
+    let table = document.createElement('table')
+    let thead = document.createElement('thead')
+    let tbody = document.createElement('tbody')
+    table.appendChild(thead)
+    table.appendChild(tbody)
+    document.getElementsByTagName("body")[0].appendChild(table)
+    for (i=0; i<8; i++){
+        let row=document.createElement('tr')
+        for (k=0; k<8; k++){
+        let eltab=document.createElement('td')
+        let eltext=document.createTextNode('TESTINA')
+        eltab.appendChild(eltext)
+        row.appendChild(eltab)
+        }
+        tbody.appendChild(row)
+    }
+    document.getElementsByTagName("body")[0].appendChild(table)
+}   
