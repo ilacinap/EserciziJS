31387bda87cb7831212eb96b505b3ee626ebb8b7
ilacinap <94383191+ilacinap@users.noreply.github.com>
2022-04-01 18:06:45 +0200

    Add files via upload

---
+++ esercizi_jq.js
@@ -0,0 +1,4 @@
+$("li").css("color","red"); //cambiare colore agli elementi della lista
+$("p").wrap("<div></div>"); //inserito un div prima e dopo il paragrafo
+$("ul").wrap("<div></div>"); //inserito un div all'inizio ed alla fine della lista
+$("li").wrap("<p></p>"); //inserito un paragrafo p prima e dopo ogni elemento della lista
